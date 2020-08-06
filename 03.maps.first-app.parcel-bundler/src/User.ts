3f613f00ef98109998ad9ad4f8032a6937d31710
DalatCoder <hieuntctk42@gmail.com>
2020-08-06 22:11:16 +0700

    03.Init commit

---
+++ 03.maps.first-app.parcel-bundler/src/User.ts
@@ -0,0 +1,19 @@
+import { name, address } from 'faker';
+
+export class User {
+  name: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+
+  constructor() {
+    this.name = name.firstName();
+
+    // Initialize location object
+    this.location = {
+      lat: parseFloat(address.latitude()),
+      lng: parseFloat(address.longitude()),
+    };
+  }
+}
