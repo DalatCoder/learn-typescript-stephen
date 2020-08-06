bc4dc54f56b8988b6e7d8a5be84536b8a71988f6
DalatCoder <hieuntctk42@gmail.com>
2020-08-06 16:41:03 +0700

    02.Complex type inside interface

--- 02.types.basic/interfaces.ts
+++ 02.types.basic/interfaces.ts
@@ -1,17 +1,23 @@
+// Create a new type with interface syntax
+// Conventions: Use generic name, capitalize first letter
+interface Vehicle {
+  name: string;
+  year: number;
+  broken: boolean;
+  summary(): string;
+}
+
 const oldCivic = {
   name: 'civic',
   year: 2000,
   broken: true,
+  summary(): string {
+    return `Name ${this.name}`;
+  },
 };
 
-const printVehicle = (vehicle: {
-  name: string;
-  year: number;
-  broken: boolean;
-}): void => {
-  console.log(`Name: ${vehicle.name}`);
-  console.log(`Year: ${vehicle.year}`);
-  console.log(`Broken: ${vehicle.broken}`);
+const printVehicle = (vehicle: Vehicle): void => {
+  console.log(vehicle.summary());
 };
 
 printVehicle(oldCivic);
