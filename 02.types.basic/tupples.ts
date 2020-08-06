8fc1c58b1f51eb904fd685dc2022f2778091f4d2
DalatCoder <hieuntctk42@gmail.com>
2020-08-06 16:24:27 +0700

    02.Tupples is not use quite often

--- 02.types.basic/tupples.ts
+++ 02.types.basic/tupples.ts
@@ -12,3 +12,11 @@ const pepsi: Drink = ['brown', true, 40];
 // pepsi[0] = 40;
 const sprite: Drink = ['clear', true, 40];
 const tea: Drink = ['brown', false, 0];
+
+// Tupples is not useful
+const carSpecs: [number, number] = [400, 3354];
+
+const carStats = {
+  horsepower: 400,
+  weight: 3344,
+};
