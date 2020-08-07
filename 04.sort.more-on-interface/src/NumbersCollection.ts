4be7106b29583068be87681bb1d7903524a6761b
DalatCoder <hieuntctk42@gmail.com>
2020-08-07 18:21:00 +0700

    04.Extract compare and swap logic to NumbersCollection

---
+++ 04.sort.more-on-interface/src/NumbersCollection.ts
@@ -0,0 +1,18 @@
+export class NumbersCollection {
+  constructor(public data: number[]) {}
+
+  get length(): number {
+    return this.data.length;
+  }
+
+  // Check if number on the left is greater than number on the right ==> Swap
+  compare(leftIndex: number, rightIndex: number): boolean {
+    return this.data[leftIndex] > this.data[rightIndex];
+  }
+
+  swap(leftIndex: number, rightIndex: number): void {
+    const leftHand = this.data[leftIndex];
+    this.data[leftIndex] = this.data[rightIndex];
+    this.data[rightIndex] = leftHand;
+  }
+}
--- 04.sort.more-on-interface/src/index.ts
+++ 04.sort.more-on-interface/src/index.ts
@@ -1,3 +1,8 @@
-const sorter = new Sorter([10, 3, -5, 0]);
+import { Sorter } from './Sorter';
+import { NumbersCollection } from './NumbersCollection';
+
+const numbers = new NumbersCollection([10, 5, 0, -3]);
+const sorter = new Sorter(numbers);
+
 sorter.sort();
-console.log(sorter.collection);
+console.log(numbers.data);
