e166414d7d9d84b43f274560df87b9d3c802c80e
DalatCoder <hieuntctk42@gmail.com>
2020-08-08 06:25:52 +0700

    04.Integrating the Sort method to the derived classes using inheritance

--- 04.sort.more-on-interface/build/Sorter.js
+++ 04.sort.more-on-interface/build/Sorter.js
@@ -2,15 +2,14 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Sorter = void 0;
 var Sorter = /** @class */ (function () {
-    function Sorter(comparable) {
-        this.comparable = comparable;
+    function Sorter() {
     }
     Sorter.prototype.sort = function () {
-        var length = this.comparable.length;
+        var length = this.length;
         for (var i = 0; i < length; i++) {
             for (var j = 0; j < length - i - 1; j++) {
-                if (this.comparable.compare(j, j + 1)) {
-                    this.comparable.swap(j, j + 1);
+                if (this.compare(j, j + 1)) {
+                    this.swap(j, j + 1);
                 }
             }
         }
--- 04.sort.more-on-interface/src/Sorter.ts
+++ 04.sort.more-on-interface/src/Sorter.ts
@@ -5,15 +5,13 @@ export interface Sortable {
 }
 
 export class Sorter {
-  constructor(public comparable: Sortable) {}
-
   sort(): void {
-    const { length } = this.comparable;
+    const { length } = this;
 
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
-        if (this.comparable.compare(j, j + 1)) {
-          this.comparable.swap(j, j + 1);
+        if (this.compare(j, j + 1)) {
+          this.swap(j, j + 1);
         }
       }
     }
