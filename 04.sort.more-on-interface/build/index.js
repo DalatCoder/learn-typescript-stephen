bd1f19df644a2e80a2b227318a52bf33bf4802db
DalatCoder <hieuntctk42@gmail.com>
2020-08-07 17:43:15 +0700

    04.Type Guard in TS to restore full access from union operator

--- 04.sort.more-on-interface/build/index.js
+++ 04.sort.more-on-interface/build/index.js
@@ -1,18 +1,27 @@
 "use strict";
 var Sorter = /** @class */ (function () {
+    // Union operator
     function Sorter(collection) {
         this.collection = collection;
     }
     Sorter.prototype.sort = function () {
         var length = this.collection.length;
-        for (var i = 0; i < length; i++) {
-            for (var j = 0; j < length - i - 1; j++) {
-                if (this.collection[j] > this.collection[j + 1]) {
-                    var temp = this.collection[j];
-                    this.collection[j] = this.collection[j + 1];
-                    this.collection[j + 1] = temp;
+        // this.collect.[methods] || only access methods that are common between number[] and string
+        // If collection is an array of numbers
+        if (this.collection instanceof Array) {
+            // Full access to medthods defined for number[]
+            for (var i = 0; i < length; i++) {
+                for (var j = 0; j < length - i - 1; j++) {
+                    if (this.collection[j] > this.collection[j + 1]) {
+                        var temp = this.collection[j];
+                        this.collection[j] = this.collection[j + 1];
+                        this.collection[j + 1] = temp;
+                    }
                 }
             }
+        }
+        // If collection is a string, do this logic instead
+        if (typeof this.collection === 'string') {
         }
     };
     return Sorter;
