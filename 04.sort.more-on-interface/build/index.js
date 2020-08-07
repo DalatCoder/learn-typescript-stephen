46c7fe7461a5495fee01fd7d05f330fe51ceb387
DalatCoder <hieuntctk42@gmail.com>
2020-08-07 22:26:58 +0700

    04.Sort a linkedlist

--- 04.sort.more-on-interface/build/index.js
+++ 04.sort.more-on-interface/build/index.js
@@ -1,13 +1,23 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var Sorter_1 = require("./Sorter");
-var CharactersCollection_1 = require("./CharactersCollection");
+var LinkedList_1 = require("./LinkedList");
 /*
 const numbers = new NumbersCollection([10, 5, 0, -3]);
 const characters = new CharactersCollection("aXaabB")
 const sorter = new Sorter(numbers);
 */
-var characters = new CharactersCollection_1.CharactersCollection('aXaabB');
-var sorter = new Sorter_1.Sorter(characters);
+/*
+const characters = new CharactersCollection('aXaabB');
+const sorter = new Sorter(characters);
 sorter.sort();
 console.log(characters.data);
+*/
+var linkedList = new LinkedList_1.LinkedList();
+linkedList.add(500);
+linkedList.add(100);
+linkedList.add(-5);
+linkedList.add(0);
+var sorter = new Sorter_1.Sorter(linkedList);
+sorter.sort();
+linkedList.print();
--- 04.sort.more-on-interface/src/index.ts
+++ 04.sort.more-on-interface/src/index.ts
@@ -1,6 +1,7 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
 /*
 const numbers = new NumbersCollection([10, 5, 0, -3]);
@@ -8,7 +9,19 @@ const characters = new CharactersCollection("aXaabB")
 const sorter = new Sorter(numbers);
 */
 
+/*
 const characters = new CharactersCollection('aXaabB');
 const sorter = new Sorter(characters);
 sorter.sort();
 console.log(characters.data);
+*/
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(100);
+linkedList.add(-5);
+linkedList.add(0);
+
+const sorter = new Sorter(linkedList);
+sorter.sort();
+linkedList.print();
