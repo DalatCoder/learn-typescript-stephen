30f41c5ece8a7c80c20a9082cc595d338c704866
DalatCoder <hieuntctk42@gmail.com>
2020-08-07 10:25:06 +0700

    03.BAD CODE!!!!!!!!!!!!!!!!!!

---
+++ 03.maps.first-app.parcel-bundler/src/CustomMap.ts
@@ -0,0 +1,39 @@
+import { User } from './User';
+import { Company } from './Company';
+
+// CustomMap class to hide google api complexity
+// Expose only neccessary methods
+
+export class CustomMap {
+  private googleMap: google.maps.Map;
+
+  constructor(divId: string) {
+    this.googleMap = new google.maps.Map(document.getElementById(divId), {
+      zoom: 1,
+      center: {
+        lat: 0,
+        lng: 0,
+      },
+    });
+  }
+
+  addNewUserMarker(user: User): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: user.location.lat,
+        lng: user.location.lng,
+      },
+    });
+  }
+
+  addNewCompanyMarker(company: Company): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: company.location.lat,
+        lng: company.location.lng,
+      },
+    });
+  }
+}
