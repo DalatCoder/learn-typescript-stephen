25a4eecd85df43c26943f47ef0d051a1c3039fc2
DalatCoder <hieuntctk42@gmail.com>
2020-08-08 17:14:22 +0700

    05.Add HtmlReport feature

--- 05.stats.csv-file/build/index.js
+++ 05.stats.csv-file/build/index.js
@@ -4,10 +4,16 @@ var CsvFileReader_1 = require("./CsvFileReader");
 var MatchReader_1 = require("./MatchReader");
 var Summary_1 = require("./Summary");
 var WinAnalysis_1 = require("./analyzers/WinAnalysis");
-var ConsoleReport_1 = require("./reportTargets/ConsoleReport");
+var HtlmReport_1 = require("./reportTargets/HtlmReport");
 var csvFileReader = new CsvFileReader_1.CsvFileReader('football.csv');
 var matchReader = new MatchReader_1.MatchReader(csvFileReader);
 matchReader.load();
 var matches = matchReader.matches;
-var analysis = new Summary_1.Summary(new WinAnalysis_1.WinAnalysis('Man United'), new ConsoleReport_1.ConsoleReport());
+/*
+const analysis = new Summary(
+  new WinAnalysis('Man United'),
+  new ConsoleReport()
+);
+*/
+var analysis = new Summary_1.Summary(new WinAnalysis_1.WinAnalysis('Man United'), new HtlmReport_1.HtmlReport());
 analysis.buildAndPrintReport(matches);
--- 05.stats.csv-file/src/index.ts
+++ 05.stats.csv-file/src/index.ts
@@ -3,6 +3,7 @@ import { MatchReader, DataReader } from './MatchReader';
 import { Summary } from './Summary';
 import { WinAnalysis } from './analyzers/WinAnalysis';
 import { ConsoleReport } from './reportTargets/ConsoleReport';
+import { HtmlReport } from './reportTargets/HtlmReport';
 
 const csvFileReader = new CsvFileReader('football.csv') as DataReader;
 const matchReader = new MatchReader(csvFileReader);
@@ -10,9 +11,13 @@ matchReader.load();
 
 const matches = matchReader.matches;
 
+/*
 const analysis = new Summary(
   new WinAnalysis('Man United'),
   new ConsoleReport()
 );
+*/
+
+const analysis = new Summary(new WinAnalysis('Man United'), new HtmlReport());
 
 analysis.buildAndPrintReport(matches);
