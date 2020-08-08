297b1b8490cc81eccf46082b63d1fe582c702f04
DalatCoder <hieuntctk42@gmail.com>
2020-08-08 11:46:39 +0700

    05.Tricky refactor using Abstract class and Generic type

--- 05.stats.csv-file/src/CsvFileReader.ts
+++ 05.stats.csv-file/src/CsvFileReader.ts
@@ -1,12 +1,14 @@
 import fs from 'fs';
+import { MatchResult } from './MatchResult';
+import { dateStringToDate } from './utils';
 
-// T represents a tupple described data on a row of the CSV file
-export abstract class CsvFileReader<T> {
-  data: T[] = [];
+// Define a tupple represents a row of CSV file (a match)
+type MatchData = [Date, string, string, number, number, MatchResult, string];
+
+export class CsvFileReader {
+  data: MatchData[] = [];
 
   constructor(public filename: string) {}
-
-  abstract mapRow(row: string[]): T;
 
   read(): void {
     this.data = fs
@@ -17,6 +19,18 @@ export abstract class CsvFileReader<T> {
       .map((row: string): string[] => {
         return row.split(',');
       })
-      .map(this.mapRow);
+      .map(
+        (row: string[]): MatchData => {
+          return [
+            dateStringToDate(row[0]),
+            row[1],
+            row[2],
+            parseInt(row[3]),
+            parseInt(row[4]),
+            row[5] as MatchResult,
+            row[6],
+          ];
+        }
+      );
   }
 }
