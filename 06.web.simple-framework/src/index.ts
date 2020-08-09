be3509c94260a2e9d746ba99808c183ce4deb369
DalatCoder <hieuntctk42@gmail.com>
2020-08-09 15:46:04 +0700

    06.Complete eventing system:

--- 06.web.simple-framework/src/index.ts
+++ 06.web.simple-framework/src/index.ts
@@ -2,8 +2,17 @@ import { User } from './models/User';
 
 const user = new User({ name: 'Hieu', age: 20 });
 
-user.on('click', () => {});
-user.on('click', () => {});
-user.on('abc', () => {});
+user.on('click', () => {
+  console.log('On click 1');
+});
+user.on('click', () => {
+  console.log('On click 2');
+});
+user.on('abc', () => {
+  console.log('On abc click');
+});
 
 console.log(user);
+
+user.trigger('click');
+user.trigger('abc');
