d0942ef8831ee63029be1b1c06dbeaa5dbf9c0bb
DalatCoder <hieuntctk42@gmail.com>
2020-08-09 16:30:05 +0700

    06.Fetch a user

--- 06.web.simple-framework/src/index.ts
+++ 06.web.simple-framework/src/index.ts
@@ -1,18 +1,6 @@
 import { User } from './models/User';
 
-const user = new User({ name: 'Hieu', age: 20 });
+const user = new User({ id: 1 });
 
-user.on('click', () => {
-  console.log('On click 1');
-});
-user.on('click', () => {
-  console.log('On click 2');
-});
-user.on('abc', () => {
-  console.log('On abc click');
-});
-
+user.fetch();
 console.log(user);
-
-user.trigger('click');
-user.trigger('abc');
