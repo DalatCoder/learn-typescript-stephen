2f1d81d173d654add6d7c22b874e285ea5b3a33b
DalatCoder <hieuntctk42@gmail.com>
2020-08-10 21:56:06 +0700

    05.Remove build folder

--- 05.stats.csv-file/build/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var MatchReader_1 = require("./MatchReader");
-var Summary_1 = require("./Summary");
-var matchReader = MatchReader_1.MatchReader.fromCsv('football.csv');
-var analysis = Summary_1.Summary.winsAnalysisWithHtmlReport('Man United');
-matchReader.load();
-analysis.buildAndPrintReport(matchReader.matches);
