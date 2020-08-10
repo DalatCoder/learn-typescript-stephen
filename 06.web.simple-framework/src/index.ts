9ad9679089eba552fe8dbab8614f38e88cb9aa2a
DalatCoder <hieuntctk42@gmail.com>
2020-08-10 10:48:01 +0700

    06.Use get syntax for delegating composition

--- 06.web.simple-framework/src/index.ts
+++ 06.web.simple-framework/src/index.ts
@@ -1,9 +1,11 @@
 import { User } from './models/User';
 
-const user = new User({ id: 1 });
+const user = new User({ name: 'Hieu', age: 20 });
 
-user.events.on('change', () => {
+user.on('change', () => {
   console.log('Change');
 });
 
-user.events.trigger('change');
+user.trigger('change');
+
+console.log(user.get('name'));
