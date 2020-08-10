29e074d74fcbf6fda13e489a530077e6c89246af
DalatCoder <hieuntctk42@gmail.com>
2020-08-10 21:24:20 +0700

    06.Strict null check

--- 06.web.simple-framework/src/index.ts
+++ 06.web.simple-framework/src/index.ts
@@ -1,11 +1,16 @@
 import { UserForm } from './views/UserForm';
 import { User } from './models/User';
 
-const userForm = new UserForm(
-  document.getElementById('root'),
-  User.buildUser({
-    name: 'NAME',
-    age: 20,
-  })
-);
-userForm.render();
+const user = User.buildUser({
+  name: 'NAME',
+  age: 20,
+});
+
+const root = document.getElementById('root');
+
+if (root) {
+  const userForm = new UserForm(root, user);
+  userForm.render();
+} else {
+  throw new Error('Root element not found');
+}
