b37af0a6d3df6e9ff9e9df647f45ac968171bc7c
DalatCoder <hieuntctk42@gmail.com>
2020-08-10 16:42:29 +0700

    06.Add static method on class User for pre-config User Collection

--- 06.web.simple-framework/src/index.ts
+++ 06.web.simple-framework/src/index.ts
@@ -1,12 +1,6 @@
-import { Collection } from './models/Collection';
-import { User, UserProps } from './models/User';
+import { User } from './models/User';
 
-const collection = new Collection<User, UserProps>(
-  'http://localhost:3000/users',
-  (data: UserProps): User => {
-    return User.buildUser(data);
-  }
-);
+const collection = User.buildUserCollection();
 
 collection.on('change', () => {
   console.log('Receive new collection');
