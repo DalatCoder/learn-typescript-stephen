7fdf0273dc8111c888e652a18b7042b4fb818b40
DalatCoder <hieuntctk42@gmail.com>
2020-08-11 09:04:21 +0700

    06.Extract View logic using inheritance

--- 06.web.simple-framework/src/views/UserForm.ts
+++ 06.web.simple-framework/src/views/UserForm.ts
@@ -1,20 +1,10 @@
-import { User } from '../models/User';
+import { View } from './View';
 
-export class UserForm {
-  constructor(public parent: Element, public model: User) {
-    this.bindModel();
-  }
-
-  bindModel(): void {
-    this.model.on('change', () => {
-      this.render();
-    });
-  }
-
+export class UserForm extends View {
   eventsMap(): { [key: string]: () => void } {
     return {
       'click:.set-age': this.onSetAgeClick,
-      'click:.set-name': this.onSetNamClick,
+      'click:.set-name': this.onSetNameClick,
     };
   }
 
@@ -22,7 +12,7 @@ export class UserForm {
     this.model.setRandomAge();
   };
 
-  onSetNamClick = (): void => {
+  onSetNameClick = (): void => {
     const input = this.parent.querySelector('input');
 
     if (input) {
@@ -42,29 +32,5 @@ export class UserForm {
         <button class="set-age">Set Random Age</button>
       </div>
     `;
-  }
-
-  bindEvents(fragment: DocumentFragment): void {
-    const eventsMap = this.eventsMap();
-
-    for (let eventKey in eventsMap) {
-      const [eventName, selector] = eventKey.split(':');
-
-      fragment.querySelectorAll(selector).forEach((element) => {
-        element.addEventListener(eventName, eventsMap[eventKey]);
-      });
-    }
-  }
-
-  render(): void {
-    // Empty parent content before rendering
-    this.parent.innerHTML = '';
-
-    const templateElement = document.createElement('template');
-    templateElement.innerHTML = this.template();
-
-    this.bindEvents(templateElement.content);
-
-    this.parent.append(templateElement.content);
   }
 }
