ffedcb5dbc0b3347b3c02d6333e69fe137e18095
DalatCoder <hieuntctk42@gmail.com>
2020-08-14 16:49:08 +0700

    02.Use decorator to catch error

--- 02.types.basic/decorators.ts
+++ 02.types.basic/decorators.ts
@@ -5,15 +5,24 @@ class Boat {
     return `This boats color is ${this.color}`;
   }
 
-  @testDecorator
+  @logError
   pilot(): void {
-    console.log('swish');
+    throw new Error();
+    // console.log('swish');
   }
 }
 
-function testDecorator(target: any, key: string): void {
-  console.log('Target', target);
-  console.log('Key', key);
+function logError(target: any, key: string, desc: PropertyDescriptor): void {
+  const method = desc.value;
+
+  desc.value = function () {
+    try {
+      method();
+    } catch (e) {
+      console.log('Oops, boat was sunk');
+    }
+  };
 }
 
 // testDecorator(Boat.prototype, 'pilot');
+new Boat().pilot();
